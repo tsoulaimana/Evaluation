62bb49ba449cf5dc0814d18a1373e8fcaf50aaf8
Thomas Soulaimana <90860978+tsoulaimana@users.noreply.github.com>
2022-11-25 12:01:02 +0100

    Add files via upload

--- Projet_Digicode/Projet_Digicode/digicode.cpp
+++ Projet_Digicode/Projet_Digicode/digicode.cpp
@@ -19,6 +19,8 @@ digicode::digicode(QWidget *parent)
     connect(ui->pushButton_9,&QPushButton::clicked,this,&digicode::onQpushButtonClicked);
     connect(ui->pushButton_Ok,&QPushButton::clicked,this,&digicode::Verifier);
 
+    CodeSecret = "1234";
+
 }
 
 digicode::~digicode()
@@ -31,18 +33,15 @@ void digicode::onQpushButtonClicked()
 
     QPushButton *pbouton = static_cast<QPushButton *>(sender());
     QString texteBouton = pbouton->text();
-     ui->lineEditMDP->setText(texteBouton);
+    CodeEntre = CodeEntre +texteBouton;
+    ui->lineEditMDP->setText(CodeEntre);
 
 }
 
 void digicode::Verifier()
 {
-    QLineEdit display;
     QMessageBox messageMdp;
     bool retour=true;
-
-        for (int i = 0; i < tailleCode && retour == true; i++)
-        {
             if (CodeEntre != CodeSecret){
                 messageMdp.setText("Code Faux");
                 retour = messageMdp.exec();
@@ -52,8 +51,6 @@ void digicode::Verifier()
                 retour = messageMdp.exec();
                 laPorte.deverrouiller();
             }
-        }
-
 }
 
 void digicode::onTimerTempoGache_timeout()
